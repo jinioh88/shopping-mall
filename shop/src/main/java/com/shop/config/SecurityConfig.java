3f476699efacded519e0acd424345a4a3ef5735b
오세진 <osejin@osejin-ui-Macmini.local>
2022-11-14 01:48:08 +0900

    간단 회원가입

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -7,14 +7,21 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
 
     @Bean
--- shop/src/main/java/com/shop/dto/MemberFormDto.java
+++ shop/src/main/java/com/shop/dto/MemberFormDto.java
@@ -2,12 +2,23 @@ package com.shop.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 public class MemberFormDto {
 
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+    @NotBlank
+    @Email
     private String email;
+    @NotEmpty
+    @Length(min = 8, max = 16)
     private String password;
+    @NotEmpty
     private String address;
 }
